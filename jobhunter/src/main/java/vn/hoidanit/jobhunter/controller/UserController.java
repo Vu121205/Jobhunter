7bed7403eeb38e645476890137c870d5b4c38cac
vuhoccode <vu03092019@gmail.com>
2026-05-20 07:27:46 +0700

    fisrt commit

---
+++ jobhunter/src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -0,0 +1,28 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class UserController {
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/user/create")
+    public String createNewUser() {
+        User user = new User();  
+        user.setEmail("john.doe@example.com");
+        user.setName("John Doe");
+        user.setPassword("password123");
+
+        this.userService.handleCreateUser(user);
+
+        return "Create new user";
+    }
+}
